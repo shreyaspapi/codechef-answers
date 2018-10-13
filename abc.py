3ef0fba6b4a45c2c13318379433967d09df20482
vidyadeepa <44111486+vidyadeepa@users.noreply.github.com>
2018-10-13 16:17:00 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,21 @@
+max1,max2 = 0,0
+lead1,lead2 = 0,0
+s,t = 0,0
+n = int(input("no of inputs: "))
+for i in range(n):
+    p1=int(input("p1: "))
+    p2=int(input("p2: "))
+    s += p1
+    t += p2
+    if s>t:
+        lead1 = s-t
+    else:
+        lead2 = t-s
+    if lead1 > max1:
+        max1 = lead1
+    elif lead2 > max2:
+        max2 = lead2
+if (max1>max2):   
+    print(1,max1)
+else:
+    print(2,max2)
