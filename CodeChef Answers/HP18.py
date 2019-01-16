cdfd8c5a3e9fcc8a3bab4c5466aca0c08df390a9
shreyaspapi <spapinwar@gmail.com>
2019-01-16 20:13:03 +0530

    Adding HP18.py

---
+++ CodeChef Answers/HP18.py
@@ -0,0 +1,26 @@
+t = int(input())
+
+for _ in range(t):
+    n, a, b = map(int, input().split())
+    s = list(map(int, input().split()))
+    count_a = 0
+    count_b = 0
+    for p in range(n):
+        if s[p]%a == 0:
+            count_a += 1
+        elif s[p]%b == 0:
+            count_b += 1
+    if count_a > count_b:
+        print("BOB")
+    elif count_b > count_a:
+        print("ALICE")
+    else:
+        print("ALICE")
+
+"""
+2
+5 3 2
+1 2 3 4 5
+5 2 4
+1 2 3 4 5
+""" 
